1ded60f7d21e317bbf8a5115c5515e641620949e
ismail sadouki <khardalodzz@gmail.com>
2021-04-01 20:11:42 -0400

    modified:   channel.html

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+    $(".first-list-element").addClass("active");
+
     $(".nav-item").click(function () {
         $(".nav-item").removeClass('active');
         $(this).addClass('active');
